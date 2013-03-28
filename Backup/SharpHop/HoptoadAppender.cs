396782e468537991da3d28a350af5734ea24f84b
Michael Eaton <m.eaton@thesnugg.com>
2013-03-28 07:46:20 +0000

    vs upgrade
    upgraded project solution files to vs2012

---
+++ Backup/SharpHop/HoptoadAppender.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using NLog;
+using NLog.Targets;
+
+
+namespace SharpHop
+{
+	
+	[Target("HoptoadAppender")] 
+    public sealed class HoptoadAppender: TargetWithLayout
+    {
+        private HoptoadClient _client;
+        private string _apiKey;
+        private string _environmentName;
+        private string _appName;
+        private string _appVersion;
+        private string _appUrl;
+		private string _aggregatorUrl;
+
+        public string ApiKey
+        {
+            get { return _apiKey; }
+            set { _apiKey = value; }
+        }
+
+        public string EnvironmentName
+        {
+            get { return _environmentName; }
+            set { _environmentName = value; }
+        }
+
+        public string AppName
+        {
+            get { return _appName; }
+            set { _appName = value; }
+        }
+
+        public string AppVersion
+        {
+            get { return _appVersion; }
+            set { _appVersion = value; }
+        }
+
+        public string AppUrl
+        {
+            get { return _appUrl; }
+            set { _appUrl = value; }
+        }
+		
+		public string AggregatorUrl
+		{
+			get { return _aggregatorUrl; }
+			set { _aggregatorUrl = value; }
+		}
+
+        public string Host { get; set; }
+		
+        public HoptoadAppender()
+        {
+			this.Host = System.Net.Dns.GetHostName();
+        }
+		
+		protected override void Write(LogEventInfo logEvent) 
+        { 
+			
+			if (logEvent.Exception == null)
+			{
+				return;
+			}
+
+			_client = new HoptoadClient(ApiKey, AggregatorUrl);
+			
+            string logMessage = logEvent.Exception.ToString();
+			
+			string stackTrace = logEvent.Exception.StackTrace.ToString();
+			string[] lines = stackTrace.Split('\n');
+
+            IList<HoptoadBackTrack> backTracks = new List<HoptoadBackTrack>();
+			foreach (var line in lines)
+			{
+				char[] delims = { ' ', ',', ':', '\t', '<','>' };
+				Console.WriteLine("line: " + line);
+				string[] words = line.Split(delims);
+				string fileName = words[10];
+				string lineNumber = words[12];
+				string methodName = words[3];
+				Console.WriteLine("file #: " + fileName);
+				Console.WriteLine("line #: " + lineNumber);
+				Console.WriteLine("method: " + methodName);
+				//line
+				//file
+				//method
+				backTracks.Add(new HoptoadBackTrack(lineNumber, fileName, methodName));
+			}
+
+
+            HoptoadMessage message = new HoptoadMessage(
+                _appName,
+                _appVersion,
+                _appUrl,
+                logEvent.Exception.GetType().ToString(),
+                logMessage,
+                backTracks,
+                "",
+                _environmentName);
+			
+			Console.WriteLine("Sending exception data to hoptoad url");
+            _client.Send(message);
+           
+
+        }
+
+    }
+}
---
+++ Backup/SharpHop/HoptoadClient.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+using System.Net;
+using System;
+using System.Xml;
+
+namespace SharpHop
+{
+	
+	public class StringWriterWithEncoding : StringWriter
+    {
+        private Encoding _encoding;
+        public StringWriterWithEncoding(StringBuilder sb, Encoding encoding)
+            : base(sb)
+        {
+            _encoding = encoding;
+        }
+        public override Encoding Encoding
+        {
+            get
+            {
+                return _encoding;
+            }
+        }
+    }
+	
+    public class HoptoadClient
+    {
+        private readonly string _apiKey;
+		private readonly string _aggregatorUrl;
+
+        public HoptoadClient(string apiKey, string aggregatorUrl)
+        {
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                throw new ArgumentNullException("apiKey");
+            }
+			
+			if (string.IsNullOrEmpty(aggregatorUrl))
+            {
+                throw new ArgumentNullException("apiKey");
+            }
+
+            _apiKey = apiKey;
+			_aggregatorUrl = aggregatorUrl;
+			
+        }
+
+        public void Send(HoptoadMessage message)
+        {
+            notice notice = new notice
+            {
+                version = "2.0",
+                apikey = _apiKey,
+                notifier = new notifier
+               {
+                   name = message.AppName,
+                   version = message.AppVersion,
+                   url = message.AppUrl
+               },
+                error = new error
+                {
+                    @class = message.ErrorClass,
+                    message = message.ErrorMessage
+                }
+            };
+
+            IList<backtraceLine> backtraceLines = new List<backtraceLine>();
+            foreach (HoptoadBackTrack backTrack in message.BackTracks)
+            {
+                backtraceLines.Add(new backtraceLine()
+                                       {
+                                           file = backTrack.FileName,
+                                           method = backTrack.MethodName,
+                                           number = backTrack.LineNumber
+                                       });
+            }
+
+            notice.error.backtrace = backtraceLines.ToArray();
+
+            notice.serverenvironment = new serverEnvironment { environmentname = message.EnvironmentName };
+
+            StringBuilder sb = new StringBuilder();
+			
+            using (StringWriter sw = new StringWriterWithEncoding(sb, Encoding.UTF8))
+            {
+
+				Console.WriteLine(sw.Encoding.EncodingName.ToString());
+				
+                XmlSerializer serializer = new XmlSerializer(typeof(notice));
+					
+                serializer.Serialize(sw, notice);
+            }
+			
+			Console.WriteLine("This is the document: " + sb.ToString());
+
+   //         HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://hoptoadapp.com/notifier_api/v2/notices");
+			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(_aggregatorUrl);
+            request.Method = "POST";
+			request.ContentType = "text/xml; encoding='utf-8'";
+            byte[] encodedData = new UTF8Encoding().GetBytes(sb.ToString());
+            
+            request.ContentLength = encodedData.Length;
+
+            using (Stream newStream = request.GetRequestStream())
+            {
+                newStream.Write(encodedData, 0, encodedData.Length);
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new HoptoadException(response.StatusCode.ToString());
+            }
+        }
+    }
+}
